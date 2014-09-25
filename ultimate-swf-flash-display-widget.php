7e20f6caa99b5a6e6efcd4fe38af86237f61ebf7
kris.lopac <kris.lopac@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-25 06:48:20 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/ultimate-swf-flash-display-widget/trunk@996477 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ultimate-swf-flash-display-widget.php
+++ ultimate-swf-flash-display-widget.php
@@ -194,7 +194,7 @@ class ultimate_swf_flash_display_widget extends WP_Widget{
 			<!--<![endif]-->
 		</object>
 		";}
-		$data .= "<div class='copy' style='color:#ccc; font-size: 9px; '><a href='http://www.crayfishcreative.com' title='click here' target='_blank'>Crayfishcreative.com</a></div>";
+		$data .= "<div class='copy' style='color:#ccc; font-size: 9px; '><a href='https://www.hawaiidrive-o.com' title='click here' target='_blank'>car rentals hawaii</a></div>";
 		echo $before_widget;
             echo $before_title . $title . $after_title;
 			echo $data;
